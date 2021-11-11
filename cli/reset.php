12fac834643a80fcb119971f27e21c38c8f4526e
Dmitrii Metelkin <dmitriim@catalyst-au.net>
2021-11-11 11:29:15 +1100

    Add CLI script for resetting assignments

--- cli/reset.php
+++ cli/reset.php
@@ -27,13 +27,21 @@ define('CLI_SCRIPT', true);
 
 require(__DIR__ . '/../../../config.php');
 require_once($CFG->libdir . '/clilib.php');
+require_once($CFG->dirroot . '/local/submissionrestict/lib.php');
 
 [$options, $unrecognized] = cli_get_params(
     [
-        'help' => false
+        'help' => false,
+        'run' => false,
+        'search' => false,
+        'hour' => false,
+        'minute' => false,
     ],
     [
-        'h' => 'help'
+        'r' => 'run',
+        's' => 'search',
+        'h' => 'hour',
+        'm' => 'minute',
     ]
 );
 
@@ -44,10 +52,15 @@ if ($unrecognized) {
 
 if ($options['help']) {
     $help = <<<EOT
-Reset submissions time.
+
+Reset submissions time for Assignment activities only.
 
 Options:
- -h, --help                Print out this help
+ --help              Print out this help
+  -r, --run          Executes reset, otherwise will be run in a dry run mode.
+  -s, --search       A string to search courses by. It will be used in "like '%{search}%'" SQL statement.
+  -h, --hour         A hour to reset submission time to (e.g. 23).
+  -m, --minute       A minutes to reset submission time to (e.g. 55).
  
 Example:
 \$sudo -u www-data /usr/bin/php local/integrity/cli/reset.php --help
@@ -55,6 +68,76 @@ Example:
 EOT;
     cli_writeln($help);
     exit(0);
+}
+
+
+if (empty($options['search'])) {
+    cli_writeln("You have to specify correct 'search' option. Please use --help to see all required options.");
+    exit(0);
+}
+
+if (empty((int)$options['hour'])) {
+    cli_writeln("You have to specify correct 'hour' option. Please use --help to see all required options.");
+    exit(0);
+}
+
+if (empty((int)$options['minute'])) {
+    cli_writeln("You have to specify correct 'minute' option. Please use --help to see all required options.");
+    exit(0);
+}
+
+// Getting all assignments where either due date or cut off date is set and the course name is what we're looking for.
+$sql = "SELECT a.id, a.name, c.fullname, a.duedate, a.cutoffdate
+          FROM {assign} a
+          JOIN {course} c ON a.course = c.id
+         WHERE " . $DB->sql_like('fullname', ':fullname', false) . " AND (a.duedate > 0 OR a.cutoffdate > 0)";
+
+$params['fullname'] = '%' . $DB->sql_like_escape($options['search']) . '%';
+
+$records = $DB->get_records_sql($sql, $params);
+
+if (empty($records)) {
+    cli_writeln("No assignments with enabled Due or Cut-off date found based on your search request");
+    exit(0);
+}
+
+$calendartype = \core_calendar\type_factory::get_calendar_instance();
+$updated = 0;
+
+foreach ($records as $record) {
+    $needupdate = false;
+    $update = new stdClass();
+    $update->id = $record->id;
+
+    if ($record->duedate > 0) {
+        if ($newdate = local_submissionrestict_calculate_new_time($record->duedate, $options['hour'], $options['minute'])) {
+            $update->duedate = $newdate;
+            $needupdate = true;
+        }
+    }
+
+    if ($record->cutoffdate > 0) {
+        if ($newdate = local_submissionrestict_calculate_new_time($record->cutoffdate, $options['hour'], $options['minute'])) {
+            $update->cutoffdate = $newdate;
+            $needupdate = true;
+        }
+    }
+
+    if ($needupdate) {
+        $updated ++;
+        if ($options['run']) {
+            $DB->update_record('assign', $update);
+            cli_writeln("Assignment $record->name with ID $record->id updated");
+        } else {
+            cli_writeln("Assignment $record->name with ID $record->id will be updated");
+        }
+    }
+}
+
+if ($options['run']) {
+    cli_writeln("$updated assignments updated");
+} else {
+    cli_writeln("$updated assignments will be updated");
 }
 
 exit(0);
